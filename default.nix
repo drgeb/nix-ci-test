36a9e6391097c2438b20fd84a34b33fcb7bd3a57
Gerry Bennett <developer@drgeb.com>
2024-01-16 15:52:01 -0600

    fix: fix build issue I believe related to spacing of the nix files
    fix build issue I believe related to spacing of the nix files
    
    BREAKING CHANGE: build not properly working on github.com

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation {
-	name = "my-project";
-	src = ./.;
-	buildInputs = [
-		nodejs
-	];
-	installPhase = ''
-		# npm install
-		touch $out
-	'';
+    name = "my-project";
+    src = ./.;
+    buildInputs = [
+      nodejs
+    ];
+    installPhase = ''
+      # npm install
+      touch $out
+    '';
 }
